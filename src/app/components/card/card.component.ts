8c307d29f561e09b4c34b90c0a39348143beefdf
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2021-03-08 21:20:02 +0200

    1

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { weather, Weathers } from '../../types/weather';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../reducers/app.reducer';
 import { selectTheme } from '../../selectors/app.selector';
 import { Observable } from 'rxjs';
 import { theme, Themes } from '../../types/theme';
+import { IWeather } from '../../types/IWeather';
 
 @Component({
   selector: 'app-card',
@@ -12,6 +13,7 @@ import { theme, Themes } from '../../types/theme';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  @Input() weather1: IWeather;
   weather: weather = Weathers.sunny;
   theme$: Observable<theme>;
   Themes = Themes;
